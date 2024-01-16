284eddbb16d31dee849fdd7aebc85ebb64b0c999
TProgramer <tposter@naver.com>
2024-01-16 15:34:00 +0900

    :sparkles: Feat: work on friend search api

--- jogakbo/src/main/java/com/noyes/jogakbo/user/DTO/Friend.java
+++ jogakbo/src/main/java/com/noyes/jogakbo/user/DTO/Friend.java
@@ -1,11 +1,13 @@
 package com.noyes.jogakbo.user.DTO;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Friend {
 
   private String nickname;
   private String socialID;
-  private String profileImageUrl;
+  private String profileImageURL;
 }
