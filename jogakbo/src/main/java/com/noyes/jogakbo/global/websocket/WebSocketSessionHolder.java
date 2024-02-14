6833280eeb26e8832720cf2c882ae71f89a06c50
TProgramer <tposter@naver.com>
2024-02-14 15:21:54 +0900

    :boom: Change session managers
    Motive: previous type of session managers were hard to close session when disconnected by closing window(need of destination).
            decided to merge two session managers into one and session close logic work for maintainance effeciency.
    Type change: Map<String, String> userSessions, Map<String, WebSocketSession> sessionIDsByDest -> Map<String, WebSocketSessionInfo> userSessions

--- jogakbo/src/main/java/com/noyes/jogakbo/global/websocket/WebSocketSessionHolder.java
+++ jogakbo/src/main/java/com/noyes/jogakbo/global/websocket/WebSocketSessionHolder.java
@@ -2,64 +2,70 @@ package com.noyes.jogakbo.global.websocket;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.List;
 import java.io.IOException;
-import java.util.ArrayList;
 
-import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
 
 public class WebSocketSessionHolder {
 
   static {
-    sessionIDsByDest = new ConcurrentHashMap<>();
     userSessions = new ConcurrentHashMap<>();
   }
 
-  // key - username, value - List of user's sessions
-  private static Map<String, List<String>> sessionIDsByDest;
-  private static Map<String, WebSocketSession> userSessions;
+  // Key 값 으로 sessionID, Value로 WebSocketInfo 를 가지는 Map 선언
+  private static Map<String, WebSocketSessionInfo> userSessions;
 
+  /**
+   * <pre>
+   * regist session into ConcurrentHashMap, userSessions.
+   * expect destination will be updated when client subscribe album.
+   * </pre>
+   * 
+   * @param session
+   */
   public static void registSession(WebSocketSession session) {
 
-    userSessions.put(session.getId(), session);
+    WebSocketSessionInfo webSocketSessionInfo = WebSocketSessionInfo.builder().session(session).build();
+    userSessions.put(session.getId(), webSocketSessionInfo);
   }
 
-  public static void addSessionByDestination(String sessionID, String destination) {
+  /**
+   * update WebSocketSessionInfo's destination field with sessionID
+   * 
+   * @param sessionID
+   * @param destination
+   */
+  public static void updateSessionWithDestination(String sessionID, String destination) {
 
-    // 처음 등록되는 destination 이면, 새로운 ArrayList 할당 후 등록
-    var sessionIDs = sessionIDsByDest.get(destination);
-
-    if (sessionIDs == null)
-      sessionIDs = new ArrayList<String>();
-
-    // destination 을 키 값으로 sessionID를 추가
-    sessionIDs.add(sessionID);
-    sessionIDsByDest.put(destination, sessionIDs);
+    // sessionID 에 해당하는 WebSocketSessionInfo 객체에 destination 필드 업데이트
+    var webSocketSessionInfo = userSessions.get(sessionID);
+    webSocketSessionInfo.setDestination(destination);
   }
 
+  /**
+   * remove WebSocketSesssionInfo in ConcurrentHashMap, userSessions
+   * 
+   * @param sessionID
+   */
   public static void removeSession(String sessionID) {
 
     userSessions.remove(sessionID);
   }
 
+  /**
+   * close WebSocketSession corresponding destination field
+   * 
+   * @param destination
+   * @throws IOException
+   */
   public static void closeSessionByDestination(String destination) throws IOException {
 
-    var sessionIDs = sessionIDsByDest.get(destination);
+    // sessionIDs를 순회하며 destination 에 해당하는 WebSocketSession을 close()
+    for (var entry : userSessions.entrySet()) {
 
-    if (sessionIDs != null) {
-      // sessionIDs를 순회하며 해당하는 WebSocketSession을 close()
-      for (var sessionID : sessionIDs) {
-        // session 이 살아있다면, userSessions 에서 제거
-        var session = userSessions.get(sessionID);
-
-        if (session != null) {
-
-          session.close(CloseStatus.GOING_AWAY);
-        }
-      }
-      // 마지막으로 sessionIDsByDest 에서 제거
-      sessionIDsByDest.remove(destination);
+      String targetDest = entry.getValue().getDestination();
+      if (targetDest != null && targetDest.equals(destination))
+        entry.getValue().getSession().close();
     }
   }
 }
